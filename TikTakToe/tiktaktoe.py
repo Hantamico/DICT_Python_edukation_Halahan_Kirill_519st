c015e55fd80a43b88beb5ce9f2610baa606067cb
Kirill <galagan.k.a.01@gmail.com>
2021-11-12 08:47:17 +0200

    TicTacToe 2-nd stage

--- TikTakToe/tiktaktoe.py
+++ TikTakToe/tiktaktoe.py
@@ -1,6 +1,22 @@
-print("X", "X", "O")
-print("0", "0", "X")
-print("X", "0", "X")
+# print("X", "X", "O")
+# print("0", "0", "X")
+# print("X", "0", "X")
 
 
+mat = input("Enter cells:").split()
+for i in range(9):
+    mat[i] = mat[i]
 
+playboard = [["", "", ""],
+             ["", "", ""],
+             ["", "", ""]]
+
+print("""
+------------
+| {} {} {} |
+| {} {} {} |
+| {} {} {} |
+------------
+""".format(mat[0], mat[1], mat[2],
+           mat[3], mat[4], mat[5],
+           mat[6], mat[7], mat[8]))

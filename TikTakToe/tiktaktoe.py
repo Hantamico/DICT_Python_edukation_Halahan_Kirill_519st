d75b5e1ac731a5605462f1e25dbb2f09a3fed698
Kirill <galagan.k.a.01@gmail.com>
2021-11-14 17:55:11 +0200

    TicTacToe 4th stage

--- TikTakToe/tiktaktoe.py
+++ TikTakToe/tiktaktoe.py
@@ -3,61 +3,97 @@
 # print("X", "0", "X")
 
 
-mat = input("Enter cells:").split()
-for i in range(9):
-    mat[i] = mat[i]
+# mat = input("Enter cells:").split()
+# for i in range(9):
+#     mat[i] = mat[i]
 
-playboard = [["", "", ""],
-             ["", "", ""],
-             ["", "", ""]]
 
-print("""
-------------
-| {} {} {} |
-| {} {} {} |
-| {} {} {} |
-------------
-""".format(mat[0], mat[1], mat[2],
-           mat[3], mat[4], mat[5],
-           mat[6], mat[7], mat[8]))
+def drawboard(mat):
+    print("-"*8)
+    print("|" + mat[0][0], mat[0][1], mat[0][2], "|")
+    print("|" + mat[1][0], mat[1][1], mat[1][2], "|")
+    print("|" + mat[2][0], mat[2][1], mat[2][2], "|")
+    print("-"*8)
 
-# horizontal X
-if mat[0:2] and mat[0] != "" and mat[0] == "X":
-    print("X Wins")
-elif mat[3] == mat[4] and mat[4] == mat[5] and mat[3] != "" and mat[3] == "X":
-    print("Game Wins")
-elif mat[6] == mat[7] and mat[7] == mat[8] and mat[6] != "" and mat[6] == "X":
-    print("Game Wins")
-# horizontal 0
-elif mat[0:2] and mat[0] != "" and mat[0] == "0":
-    print("0 Wins")
-elif mat[3] == mat[4] and mat[4] == mat[5] and mat[3] != "" and mat[3] == "0":
-    print("0 Wins")
-elif mat[6] == mat[7] and mat[7] == mat[8] and mat[6] != "" and mat[6] == "0":
-    print("0 Wins")
-# vertical X
-elif mat[0] == mat[3] and mat[3] == mat[6] and mat[0] != "" and mat[0] == "X":
-    print("X Wins")
-elif mat[1] == mat[4] and mat[4] == mat[7] and mat[1] != "" and mat[1] == "X":
-    print("X Wins")
-elif mat[2] == mat[5] and mat[5] == mat[8] and mat[2] != "" and mat[2] == "X":
-    print("X Wins")
-# verical 0
-elif mat[0] == mat[3] and mat[3] == mat[6] and mat[0] != "" and mat[0] == "0":
-    print("0 Wins")
-elif mat[1] == mat[4] and mat[4] == mat[7] and mat[1] != "" and mat[1] == "0":
-    print("0 Wins")
-elif mat[2] == mat[5] and mat[5] == mat[8] and mat[2] != "" and mat[2] == "0":
-    print("0 Wins")
-# diagonal
-elif mat[0] == mat[4] and mat[4] == mat[8] and mat[0] != "" and mat[0] == "X":
-    print("X Wins")
-elif mat[2] == mat[4] and mat[4] == mat[6] and mat[2] != "" and mat[2] == "X":
-    print("X Wins")
-# diagonal 0
-elif mat[0] == mat[4] and mat[4] == mat[8] and mat[0] != "" and mat[0] == "0":
-    print("0 Wins")
-elif mat[2] == mat[4] and mat[4] == mat[6] and mat[2] != "" and mat[2] == "0":
-    print("0 Wins")
-elif mat[0:9] != "":
-    print("Game Draw")
+
+def check_row(mat, row):
+    return (mat[row][0] == mat[row][1] and mat[row][1] == mat[row][2] and mat[row][0] != " ")
+
+
+def check_column(mat, col):
+    return (mat[0][col] == mat[1][col] and mat[1][col] == mat[2][col] and mat[0][col] != " ")
+
+
+def check_diagonals(mat):
+    return (mat[0][0] == mat[1][1] and mat[1][1] == mat[2][2] and mat[0][0] != " ") or \
+           (mat[2][0] == mat[1][1] and mat[1][1] == mat[0][2] and mat[2][0] != " ")
+
+
+def check_winner(mat):
+    for i in range(3):
+        if check_row(mat, i):
+            return True
+        if check_column(mat, i):
+            return True
+    if check_diagonals(mat):
+        return True
+    return False
+
+
+def is_mat_full(mat):
+    for item in mat:
+        if " " in item:
+            return False
+    return True
+
+
+def play(mat):
+    while True:
+        row = input("Enter row number: ")
+        while not row.isdigit() or int(row) < 1 or int(row) > 3:
+            row = input("Enter row number between 1-3: ")
+        row = int(row)
+        col = input("Enter column number: ")
+        while not col.isdigit() or int(col) < 1 or int(col) > 3:
+            col = input("Enter column number between 1-3: ")
+        col = int(col)
+        if mat[row - 1][col - 1] != " ":
+            print("Pick an empty box!")
+        else:
+            return (row - 1, col - 1)
+
+
+def main():
+    print("\n== Tic Tac Toe ==")
+    mat = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]
+    ]
+
+    players = ["X", "O"]
+
+    turn = 0
+    while not is_mat_full(mat):
+        drawboard(mat)
+        if turn == 0:
+            print("You play!")
+            row, col = play(mat)
+            mat[row][col] = players[turn]
+
+        else:
+            print("Computer plays!")
+            row, col = play(mat)
+            mat[row][col] = players[turn]
+
+        if check_winner(mat):
+            drawboard(mat)
+            print("X won!" if turn == 0 else "0 won!")
+            break
+        turn = 1 - turn
+    else:
+        drawboard(mat)
+        print("It's a tie!")
+
+
+main()

0bbfce21b479cdd49ed3ec1331b76895e1a7d7f7
Kirill <galagan.k.a.01@gmail.com>
2021-11-24 16:12:09 +0200

    Credit calculator 3-d stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -3,21 +3,55 @@ import math
 
 def main():
     principal = int(input("Enter the loan principal: "))
-    print("What do you want to calculate?")
-    mp = input("type 'p' – for the monthly payment\ntype 'm' – for number of monthly payments")
-
-    if mp == "p":
-        month = int(input("enter the number of month\n>"))
-        pay = math.ceil(principal/month)
-        lastpay = math.ceil(principal - (month - 1)*pay)
-        if lastpay != pay:
-            print("Your monthly payment was", pay, "and last payment was", lastpay)
+    mp = input(f"""What do you want to calculate?
+     type "n" for number of monthly payments,
+     type "a" for annuity monthly payment amount,
+     type "p" for loan principal,
+     type "exit" for exit:\n >""")
+    while True:
+        if mp == "p":
+            p()
+        elif mp == "a":
+            a(principal)
+        elif mp == "n":
+            n(principal)
+        elif mp == "exit":
+            exit()
         else:
-            print("Your monthly payment was", pay)
-    elif mp == "m":
-        m = int(input("enter the monthly payment"))
-        mm = math.ceil(principal//m)
-        print("it will take", mm, "month to repay the loan")
+            main()
+
+
+def p():
+    annuity = float(input("Enter the annuity payment:\n"))
+    periods = int(input("Enter the number of periods:\n"))
+    loan_i = float(input("Enter the loan interest:\n"))
+    cal = (loan_i * 0.01 / 12)
+    num = pow(1 + cal, periods)
+    result_p = annuity / ((cal * num) / (num - 1))
+    print("Your loan principal =", math.floor(result_p))
+
+
+def a(principal):
+    per = int(input("Enter the number of periods:\n"))
+    li = float(input("Enter the loan interest:\n"))
+    cal = (li * 0.01 / 12)
+    num = pow(1 + cal, per)
+    ra = principal * ((cal * num) / (num - 1))
+    print("Your monthly payment =", math.ceil(ra))
+
+
+def n(principal):
+    monthly_payment = int(input("enter the monthly payment:"))
+    loan_interests = float(input("enter the loan interests"))
+    cal = (loan_interests * 0.01 / 12)
+    fraction = (monthly_payment / (monthly_payment - cal * principal))
+    res_n = math.ceil(math.log(fraction, cal + 1))
+    years = res_n / 12
+    month = (years - math.floor(years)) * 12
+    if math.floor(years) != 0:
+        print("It will take", math.floor(years), "years and", math.ceil(month), "months to repay this loan!\n>")
+    else:
+        print("Your monthly payment =", month)
 
 
 if __name__ == '__main__':

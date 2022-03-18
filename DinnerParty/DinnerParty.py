e396e4a9e906f19b3715c8c3ab62f4cb6f775df8
Kirill <galagan.k.a.01@gmail.com>
2022-03-18 17:08:12 +0200

    DinnerParty stage 1

---
+++ DinnerParty/DinnerParty.py
@@ -0,0 +1,11 @@
+print('Enter the number of friends joining (including you):')
+number_of_friends = int(input())
+members = {}
+if number_of_friends >= 0:
+    print('Enter the name of every friend (including you), each on a new line:')
+    for i in range(number_of_friends):
+        name = input(">")
+        members[name] = 0
+else:
+    print('No one is joining for the party')
+print(members)

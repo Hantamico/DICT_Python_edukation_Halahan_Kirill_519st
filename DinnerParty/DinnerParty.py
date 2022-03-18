62132c4641230651c3f9a1ebe63d77116b294951
Kirill <galagan.k.a.01@gmail.com>
2022-03-18 17:40:47 +0200

    DinnerParty stage 3

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,16 +1,24 @@
-print('Enter the number of friends joining (including you):')
-number_of_friends = int(input())
+from random import choice
+
+
+number_of_friends = int(input('Enter the number of friends joining (including you):'))
 members = {}
+members_list = []
 if number_of_friends >= 0:
     print('Enter the name of every friend (including you), each on a new line:')
     for i in range(number_of_friends):
         name = input(">")
         members[name] = 0
+        members_list.append(name)
 else:
     print('No one is joining for the party')
 print('Enter the total amount:')
 all_amount = int(input())
-amount = round(all_amount / number_of_friends, 2)
 for name in members:
-    members[name] = amount
-print(members)
+    members[name] = round(all_amount / number_of_friends, 2)
+r_name = choice(members_list)
+answer = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
+if answer == 'Yes':
+    print(r_name + ' is the lucky one!')
+else:
+    print('No one is going to be lucky')

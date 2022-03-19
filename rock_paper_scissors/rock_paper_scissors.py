971690394eb8389dcecffe9dce4b361c096391cb
Kirill <galagan.k.a.01@gmail.com>
2022-03-19 16:20:44 +0200

    rock_paper_scissors stage 1

---
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,7 @@
+choose = input("choose one of option: \n -Rock \n -Paper \n -Scissors \n -------------------\n>")
+if choose == "Rock":
+    print("Sorry, but the computer chose paper")
+elif choose == "Paper":
+    print("Sorry, but the computer chose scissors")
+elif choose == "Scissors":
+    print("Sorry, but the computer chose rock")

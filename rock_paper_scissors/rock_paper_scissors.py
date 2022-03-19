f473f7fbcf24fb4321ad0bd4336200bc18cf3adf
Kirill <galagan.k.a.01@gmail.com>
2022-03-19 17:39:09 +0200

    rock_paper_scissors stage 3

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -4,24 +4,29 @@ import random
 while True:
     choose = input("choose one of option: \n -Rock \n -Paper \n -Scissors \n -------------------\n>")
     combo_list = ["Rock", "Paper", "Scissors"]
-    bot_choise = random.choice(combo_list)
-    print(bot_choise)
-    if bot_choise == "Rock" and choose == "Scissors":
-        print("LOSE - Sorry, but the computer chose Rock")
-    if bot_choise == "Rock" and choose == "Rock":
-        print("DRAW - There is a draw Rock")
-    if bot_choise == "Rock" and choose == "Paper":
-        print("WIN - Well done. The computer chose Rock and failed")
-    if bot_choise == "Paper" and choose == "Scissors":
-        print("WIN - Well done. The computer chose Paper and failed")
-    if bot_choise == "Paper" and choose == "Paper":
-        print("DRAW - There is a draw Paper")
-    if bot_choise == "Paper" and choose == "Rock":
-        print("LOSE - Sorry, but the computer chose Paper")
-    if bot_choise == "Scissors" and choose == "Scissors":
-        print("DRAW - There is a draw Scissors")
-    if bot_choise == "Scissors" and choose == "Rock":
-        print("WIN - Well done. The computer chose Scissors and failed")
-    if bot_choise == "Scissors" and choose == "Paper":
-        print("LOSE - Sorry, but the computer chose Scissors")
-
+    if choose in combo_list:
+        bot_choise = random.choice(combo_list)
+        print(bot_choise)
+        if bot_choise == "Rock" and choose == "Scissors":
+            print("LOSE - Sorry, but the computer chose Rock")
+        if bot_choise == "Rock" and choose == "Rock":
+            print("DRAW - There is a draw Rock")
+        if bot_choise == "Rock" and choose == "Paper":
+            print("WIN - Well done. The computer chose Rock and failed")
+        if bot_choise == "Paper" and choose == "Scissors":
+            print("WIN - Well done. The computer chose Paper and failed")
+        if bot_choise == "Paper" and choose == "Paper":
+            print("DRAW - There is a draw Paper")
+        if bot_choise == "Paper" and choose == "Rock":
+            print("LOSE - Sorry, but the computer chose Paper")
+        if bot_choise == "Scissors" and choose == "Scissors":
+            print("DRAW - There is a draw Scissors")
+        if bot_choise == "Scissors" and choose == "Rock":
+            print("WIN - Well done. The computer chose Scissors and failed")
+        if bot_choise == "Scissors" and choose == "Paper":
+            print("LOSE - Sorry, but the computer chose Scissors")
+    elif choose == "exit":
+        print("Bye!")
+        exit()
+    else:
+        print("Invalid input")

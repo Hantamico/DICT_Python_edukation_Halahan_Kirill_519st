50612ce48d82b99c04a90ea283c2177c8a060a3e
Kirill <galagan.k.a.01@gmail.com>
2022-04-12 15:27:11 +0300

    Currency_Exchange.py stage4

--- currency-exchange/currency_exchange.py
+++ currency-exchange/currency_exchange.py
@@ -1,56 +1,72 @@
 import requests
 import json
-from functools import lru_cache
 
 
-def get_rate(code):
-    print("Searching...")
-    res = requests.get(f"http://www.floatrates.com/daily/{code}.json")
-    if res.ok:
-        result = res.json()
-        d = result.get("usd").get("rate")
-        e = result.get("eur").get("rate")
-        return round(d, 2), round(e, 2)
-    else:
-        return
+class CurrencyExchange:
 
+    def __init__(self):
+        self.cache = dict()
+        self.currency_for_exchange = None
 
-def step_3():
-    code = input("> ").lower()
-    results = get_rate(code)
-    if not results:
-        print(f'No results found for "{code}"')
-        step_3()
-        return
+    def check_cache(self, func, currency):
 
-    dollar, euro = results
-    print(f"""Dollar: {round(dollar, 2)}
-Euro: {round(euro, 2)}""")
-    step_3()
+        print("Checking the cache...")
+        if currency in self.cache:
+            print("It is in the cache!")
+            return self.cache[currency]
 
+        print("Sorry, but it is not in the cache!")
+        result = func(currency)
+        self.cache[currency] = result
 
-def correct_float_input(string):
-    user_input = input(string)
-    while True:
-        try:
-            float(user_input)
-        except ValueError:
-            if "," in user_input:
-                user_input = user_input.replace(",", ".")
+        return result
+
+    def get_exchange_rate(self, currency):
+        print(f"Wait... ")
+        response = requests.get(f"http://www.floatrates.com/daily/{self.currency_for_exchange}.json")
+        if response.ok:
+            result = response.json()
+
+            try:
+
+                result = result.get(f"{currency}").get("rate")
+            except AttributeError:
+                return
+
+            return round(result, 2)
+
+    def menu(self):
+        self.currency_for_exchange = input("Currency for exchange > ").lower()
+        self.play_currency_exchange()
+
+    def play_currency_exchange(self):
+        amount_of_many = self.correct_float_input("Amount of many > ")
+        currency = input("What to change > ").lower()
+        exchange_rate = self.check_cache(self.get_exchange_rate, currency)
+        if not exchange_rate:
+            print("Incorrect parameters")
+            self.play_currency_exchange()
+            return
+        print(f"You received {round(amount_of_many * exchange_rate, 2)} {currency.upper()}.")
+        self.play_currency_exchange()
+
+    @staticmethod
+    def correct_float_input(string):
+        user_input = input(string)
+        while True:
+            try:
+                float(user_input)
+            except ValueError:
+                if "," in user_input:
+                    user_input = user_input.replace(",", ".")
+                    continue
+                print("Incorrect format")
+                user_input = input(string)
                 continue
-            print("Incorrect format")
-            user_input = input(string)
-            continue
-        else:
-            break
-    return float(user_input)
+            else:
+                break
+        return float(user_input)
 
 
-step_3()
-
-
-# coin = float(input("Please, enter the number of mycoins you have: >"))
-# print("I will get", coin * 0.82, "ARS from the sale of", coin, "mycoins")
-# print("I will get", coin * 0.17, "HNL from the sale of", coin, "mycoins")
-# print("I will get", coin * 1.96, "AUD from the sale of", coin, "mycoins")
-# print("I will get", coin * 0.208, "MAD from the sale of", coin, "mycoins")
+currency_ex = CurrencyExchange()
+currency_ex.menu()
